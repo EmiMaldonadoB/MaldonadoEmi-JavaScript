d5b01affc7543b61900a91ee461087239680d51c
EmiMaldonadoB <109559421+EmiMaldonadoB@users.noreply.github.com>
2022-10-25 13:42:46 -0300

    fixie errores

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -6,84 +6,84 @@ const productos = [
         id: 100,
         nombre: "Bizcochos Don Satur 200grs",
         precio: 200,
-        imgSrc: "/Imagenes/100.webp",
+        imgSrc: "./Imagenes/100.webp",
         cantidad: 1
     },
     {
         id: 101,
         nombre: "Galletas Oreo 118gr",
         precio: 180,
-        imgSrc: "/Imagenes/101.webp",
+        imgSrc: "./Imagenes/101.webp",
         cantidad: 1
     },
     {
         id: 102,
         nombre: "Galletas Surtido Bagley 390gr",
         precio: 470,
-        imgSrc: "/Imagenes/102.webp",
+        imgSrc: "./Imagenes/102.webp",
         cantidad: 1
     },
     {
         id: 103,
         nombre: "Pringles Queso 71gr",
         precio: 300,
-        imgSrc: "/Imagenes/103.webp",
+        imgSrc: "./Imagenes/103.webp",
         cantidad: 1
     },
     {
         id: 104,
         nombre: "Papas Lays Clasicas 45gr",
         precio: 180,
-        imgSrc: "/Imagenes/104.webp",
+        imgSrc: "./Imagenes/104.webp",
         cantidad: 1
     },
     {
         id: 105,
         nombre: "Pringles Cebolla y Crema 71gr",
         precio: 300,
-        imgSrc: "/Imagenes/105.webp",
+        imgSrc: "./Imagenes/105.webp",
         cantidad: 1
     },
     {
         id: 106,
         nombre: "Coca Cola 1.5lts",
         precio: 250,
-        imgSrc: "/Imagenes/106.webp",
+        imgSrc: "./Imagenes/106.webp",
         cantidad: 1
     },
     {
         id: 107,
         nombre: "Aquarius Pomelo 2.25lts",
         precio: 320,
-        imgSrc: "/Imagenes/107.webp",
+        imgSrc: "./Imagenes/107.webp",
         cantidad: 1
     },
     {
         id: 108,
         nombre: "H2o Citrus 2.25lts",
         precio: 320,
-        imgSrc: "/Imagenes/108.webp",
+        imgSrc: "./Imagenes/108.webp",
         cantidad: 1
     },
     {
         id: 109,
         nombre: "Cerveza Patagonia 730cc",
         precio: 350,
-        imgSrc: "/Imagenes/109.webp",
+        imgSrc: "./Imagenes/109.webp",
         cantidad: 1
     },
     {
         id: 110,
         nombre: "Cerveza Heineken 730cc",
         precio: 330,
-        imgSrc: "/Imagenes/110.webp",
+        imgSrc: "./Imagenes/110.webp",
         cantidad: 1
     },
     {
         id: 111,
         nombre: "Cerveza Corona 740 cc",
         precio: 390,
-        imgSrc: "/Imagenes/111.webp",
+        imgSrc: "./Imagenes/111.webp",
         cantidad: 1
     },
 
